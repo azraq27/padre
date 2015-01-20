f6e257dc6337249558ce066a13c27c306770a0c6
Bill Gross <bill.gross@me.com>
2015-01-20 02:32:12 -0600

    version

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
           "views/style/tab_selected.png"
       ])
   ],
-  version = '2.1.3',
+  version = '2.1.4',
   description = 'participant data repository',
   long_description = '''PaDRe - a participant data repository library (in Python)
 --------------------------------------------------------
@@ -44,7 +44,7 @@ Documentation: http://padre.readthedocs.org
   author = 'Bill Gross',
   author_email = 'bill.gross@me.com',
   url = 'https://github.com/azraq27/padre',
-  download_url = 'https://github.com/azraq27/padre/tarball/2.1.3',
+  download_url = 'https://github.com/azraq27/padre/tarball/2.1.4',
   keywords = ['neuroimaging', 'afni', 'fsl', 'fmri'],
   classifiers = [
       'Topic :: Scientific/Engineering',

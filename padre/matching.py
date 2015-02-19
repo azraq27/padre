e35eb964007355ffd3533d164483fdcdd64cacd4
Bill Gross <bill.gross@me.com>
2015-02-19 11:39:11 -0600

    still haven't fully figured out how to implement gini

--- padre/matching.py
+++ padre/matching.py
@@ -1,37 +1,39 @@
 '''uses methods from the ``gini`` library for flexible matching'''
-import gini
+from gini.semantics import Bottle,Concept
 import padre as p
 import neural as nl
 import copy
 
-bottle = gini.semantics.Bottle()
+bottle = Bottle()
 
 p.subjects()
-bottle.vocab = {
+bottle.vocab = [
     # Direct objects
-    'atlas_do': ['atlases','templates'],
-    'subject_do': ['subjects','patients'],
-    'experiment_do': ['experiments', 'studies', 'study'],
-    'label_do': ['labels','tasks'],
-    'tag_do': ['types','tags'],
-    'session_do': ['sessions','scans','dates'],
-    'dset_do': ['dsets','datasets','runs','files'],
-    'metadata_do': ['meta','metadata','behavioral','eprime'],
-    'eprime_do': ['eprime','edat'],
-    'eprime-txt_do': ['eprime-txt'],
+    Concept('atlas_do', ['atlases','templates']),
+    Concept('subject_do', ['subjects','patients']),
+    Concept('experiment_do', ['experiments', 'studies', 'study']),
+    Concept('label_do', ['labels','tasks']),
+    Concept('tag_do', ['types','tags']),
+    Concept('session_do', ['sessions','scans','dates']),
+    Concept('dset_do', ['dsets','datasets','runs','files']),
+    Concept('metadata_do', ['meta','metadata','behavioral','eprime']),
+    Concept('eprime_do', ['eprime','edat']),
+    Concept('eprime-txt_do', ['eprime-txt']),
     # Actions
-    'list': ['list','show','get','print'],
-    'link': ['link','copy','symlink'],
-    'add': ['add','attach','upload'],
+    Concept('list', ['list','show','get','print']),
+    Concept('link', ['link','copy','symlink']),
+    Concept('add', ['add','attach','upload']),
     # Objects
-    'subject': p.subject._all_subjects.keys(),
-    'label': p.subject.tasks,
-    'tag': p.subject.tags,
-    'experiment': p.subject.experiments,
-    'quiet': ['quiet','quietly','-q','--quiet']
-}
+    Concept('subject', p.subject._all_subjects.keys()),
+    Concept('label', p.subject.tasks),
+    Concept('tag', p.subject.tags),
+    Concept('experiment', p.subject.experiments),
+    Concept('quiet', ['quiet','quietly','-q','--quiet'])
+]
+
 
 def recursive_parse_string(bottle,string):
+    raise RuntimeError('Need to update to newest gini version')
     '''calls :meth:`bottle.parse_string` and then adds subject-specific information and re-runs it to get things like dataset names'''
     matches = bottle.parse_string(string)
     if 'other' in matches:
@@ -43,6 +45,7 @@ def recursive_parse_string(bottle,string):
         # Would be nice to keep track of where they came from (what subject is this dset from?)
 
 def dsets_with(subj,args):
+    raise RuntimeError('Need to update to newest gini version')
     '''returns the result of :meth:`Subject.dsets`, using the relevant keyword arguments from args'''
     def get(dict,key,only_one=True):
         if key in dict:
@@ -54,47 +57,35 @@ def dsets_with(subj,args):
             return None
     return subj.dsets(label=get(args,'label'),tags=get(args,'tag',False),experiment=get(args,'experiment'))
 
-def filter_subjs(subjects,args):
-    if not any([x in args for x in ['subject','session','label','experiment','tag']]):
+def filter_subjs(subjects,matches):
+    if not any([x in matches for x in ['subject','session','label','experiment','tag']]):
         nl.notify('Using no constraints')
         return subjects
     with nl.notify('Using constraints:'):
-        if 'subject' in args:
-            nl.notify('subjects = %s' % repr(args['subject']))
-            subjects = [x for x in subjects if str(x) in args['subject']]
-        if 'session' in args:
-            nl.notify('sessions = %s' % repr(args['session']))
-            subjects = [x for x in subjects if any([y in x.sessions for y in args['session']])]
-        if 'experiment' in args:
-            nl.notify('experiments = %s' % repr(args['experiment']))
-            new_subjects = []
-            for subj in subjects:
-                for sess in subj.sessions:
-                    if 'experiment' in subj._sessions[sess] and subj._sessions[sess]['experiment'] in args['experiment']:
-                        if subj not in new_subjects:
-                            new_subjects.append(subj) 
-                        break
-            subjects = new_subjects
-        if 'label' in args:
-            nl.notify('labels = %s' % repr(args['label']))
-            new_subjects = []
-            for label in args['label']:
-                for subj in subjects:
-                    for sess in subj.sessions:
-                        if label in subj._sessions[sess]['labels']:
-                            if subj not in new_subjects:
-                                new_subjects.append(subj) 
-                            break
-            subjects = new_subjects
-        if 'tag' in args:
-            nl.notify('tags = %s' % repr(args['tag']))
-            new_subjects = []
-            for tag in args['tag']:
-                for subj in subjects:
-                    for sess in subj.sessions:
-                        if 'tags' in subj._sessions[sess] and tag in subj._sessions[sess]['tags']:
-                            if subj not in new_subjects:
-                                new_subjects.append(subj) 
-                            break
-            subjects = new_subjects
+        for concept in matches:
+            examples = concept.examples_matching()
+            nl.notify('%s = %s' % (concept.name,repr(examples)))
+            if concept=='subject':
+                subjects = [x for x in subjects if str(x) in examples]
+            if concept=='session':
+                # This isn't going to work unless we've done a recursive match
+                subjects = [x for x in subjects if any([y in x.sessions for y in examples])]
+            loop_tests = {
+                'experiment': lambda subj,sess: subj._sessions[sess]['experiment'] in examples,
+                'label': lambda subj,sess: all([x in subj._sessions[sess]['labels'] for x in examples]),
+                'tag': lambda subj,sess: all([x in subj._sessions[sess]['tags'] for x in examples])
+            }
+            for attr in loop_tests:
+                if concept==attr:
+                    new_subjects = []
+                    for subj in subjects:
+                        for sess in subj.sessions:
+                            try:
+                                if loop_tests[attr](subj,sess):
+                                    if subj not in new_subjects:
+                                        new_subjects.append(subj) 
+                                    break
+                            except:
+                                pass
+                    subjects = new_subjects
     return subjects

0fffb272c6d932b5dc7b4a8a6dcec620401f7c68
Bill Gross <bill.gross@me.com>
2015-03-01 16:20:41 -0600

    updated search to use newest version of gini

--- padre/matching.py
+++ padre/matching.py
@@ -1,5 +1,6 @@
 '''uses methods from the ``gini`` library for flexible matching'''
 from gini.semantics import Bottle,Concept
+from gini import logic
 import padre as p
 import neural as nl
 import copy
@@ -24,10 +25,10 @@ bottle.vocab = [
     Concept('link', ['link','copy','symlink']),
     Concept('add', ['add','attach','upload']),
     # Objects
-    Concept('subject', p.subject._all_subjects.keys()),
-    Concept('label', p.subject.tasks),
-    Concept('tag', p.subject.tags),
-    Concept('experiment', p.subject.experiments),
+    Concept('subject', [[x] for x in p.subject._all_subjects.keys()]),
+    Concept('label', [[x] for x in p.subject.tasks]),
+    Concept('tag', [[x] for x in p.subject.tags]),
+    Concept('experiment', [[x] for x in p.subject.experiments]),
     Concept('quiet', ['quiet','quietly','-q','--quiet'])
 ]
 
@@ -57,35 +58,62 @@ def dsets_with(subj,args):
             return None
     return subj.dsets(label=get(args,'label'),tags=get(args,'tag',False),experiment=get(args,'experiment'))
 
-def filter_subjs(subjects,matches):
-    if not any([x in matches for x in ['subject','session','label','experiment','tag']]):
-        nl.notify('Using no constraints')
-        return subjects
+def filter_by_match(match,subjects):
+    if match.concept.name=='subject':
+        # Combine subjects by OR (AND doesn't make any sense..)
+        return [x for x in subjects if str(x)==match.examples]
+    if match.concept.name=='experiment':
+        # Combine experiments by OR
+        filtered = []
+        for subj in subjects:
+            if any([subj._sessions[sess]['experiment'] in match.examples for sess in subj.sessions if 'experiment' in subj._sessions[sess] and subj._sessions[sess]['experiment']]):
+                filtered.append(subj)
+        return filtered
+    if match.concept.name=='label':
+        # Combine with AND per subject (not necessarily the same session)
+        filtered = []
+        for subj in subjects:
+            if any([any([label in match.examples for label in subj._sessions[sess]['labels']]) for sess in subj.sessions]):
+                filtered.append(subj)
+        return filtered
+    if match.concept.name=='tag':
+        # Combine with AND per subject (not necessarily the same session)
+        filtered = []
+        for subj in subjects:
+            if any([any([tag in match.examples for tag in subj._sessions[sess]['tags']]) for sess in subj.sessions if 'tags' in subj._sessions[sess]]):
+                filtered.append(subj)
+        return filtered
+
+def filter_subjs(subjects=None,string=None,matches=None,require_match=True):
+    '''takes ``list`` of subjects and filters based on the :class:`ConceptMatch`s given in ``matches``. If ``matches``
+    is not given, will parse the string ``string`` instead. If ``require_match``, will return ``None`` if it fails to find
+    any constraints, otherwise it returns ``subjects``'''
+    match_concepts = ['subject','session','label','experiment','tag']
+    if subjects==None:
+        subjects = p.subjects()
+    if matches==None:
+        if string==None:
+            return subjects
+        matches = bottle.parse_string(string)
+    running_exclusions = {}
+    if not any([[x in sub_matches for x in match_concepts] for sub_matches in matches]):
+        if require_match:
+            nl.notify('Warning: Could not find any constraints to use',level=nl.level.warning)
+            return None
+        else:
+            nl.notify('Using no constraints')
+            return subjects
     with nl.notify('Using constraints:'):
-        for concept in matches:
-            examples = concept.examples_matching()
-            nl.notify('%s = %s' % (concept.name,repr(examples)))
-            if concept=='subject':
-                subjects = [x for x in subjects if str(x) in examples]
-            if concept=='session':
-                # This isn't going to work unless we've done a recursive match
-                subjects = [x for x in subjects if any([y in x.sessions for y in examples])]
-            loop_tests = {
-                'experiment': lambda subj,sess: subj._sessions[sess]['experiment'] in examples,
-                'label': lambda subj,sess: all([x in subj._sessions[sess]['labels'] for x in examples]),
-                'tag': lambda subj,sess: all([x in subj._sessions[sess]['tags'] for x in examples])
-            }
-            for attr in loop_tests:
-                if concept==attr:
-                    new_subjects = []
-                    for subj in subjects:
-                        for sess in subj.sessions:
-                            try:
-                                if loop_tests[attr](subj,sess):
-                                    if subj not in new_subjects:
-                                        new_subjects.append(subj) 
-                                    break
-                            except:
-                                pass
-                    subjects = new_subjects
-    return subjects
+        subjects_total = set(subjects)
+        for match_options in matches:
+            if not isinstance(match_options,list):
+                continue
+            match_options = [x for x in match_options if x in match_concepts]
+            if len(match_options)==0:
+                continue
+            subjects_options = set()
+            for match in match_options:
+                nl.notify('%s = %s' % (match.concept.name,repr(match.examples)))
+                subjects_options = logic.or_sets(subjects_options,set(filter_by_match(match,subjects)))
+            subjects_total = logic.and_sets(subjects_options,subjects_total)
+    return subjects_total
